d66a58349ef9de886485b20a5170416f38a90dff
Andrej <GjgjdF007@yandex.ru>
2025-02-25 12:53:02 +0300

    add error handling, restucturing form

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
-import { Button, TextInput, View } from 'react-native';
+/* eslint-disable no-lone-blocks */
+import React, { FC } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { style } from './FormStyle';
-import { Controller, useForm } from 'react-hook-form';
+import { Control, Controller, FieldErrors, useForm } from 'react-hook-form';
 
 type Inputs = {
     login: string,
     password: string,
 }
+
+type ControlProps = {
+    control: Control<Inputs, any>,
+    errors: FieldErrors<Inputs>,
+    name: 'login' | 'password',
+    rules: {},
+    placeholder: string
+}
+
+// console.log(String({...Inputs}.keys));
+
+const ControlForm: FC<ControlProps> = ({ control, errors, name, rules, placeholder }) => {
+
+    return (
+        <>
+            <Controller
+                name={name}
+                control={control}
+                rules={{ ...rules }}
+                render={({ field: { onChange, value } }) => (
+                    <TextInput
+                        style={[
+                            style.form,
+                            errors[name] ? style.form_error : {},
+                        ]}
+                        placeholder={placeholder}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+            />
+            {
+                errors[name]
+                &&
+                <Text style={style.error_text}>
+                    {errors[name]?.message}
+                </Text>
+            }
+        </>
+
+    );
+};
 
 export const Form = () => {
     const navigation = useNavigation();
@@ -15,49 +58,23 @@ export const Form = () => {
     const { control, handleSubmit, formState: { errors } } = useForm<Inputs>();
     const onSubmit = (data: Inputs) => {
         console.log(data);
-        if (data.login === 'admin' && data.password === '1234'){
+        if (data.login === 'admin' && data.password === '1234') {
             navigation.navigate('Todo');
         }
+        else {
+            console.error('Invalid name or password');
+            console.log(errors);
+        }
     };
-    console.log(errors);
 
     return (
         <View>
-            <Controller
-                name="login"
-                control={control}
-                rules={{
-                    required: true,
-                }}
-                render={({ field: { onChange, value } }) => (
-                    <TextInput
-                        style={style.form}
-                        placeholder="Login"
-                        onChangeText={onChange}
-                        value={value}
-                    />
-                )}
-            />
-            <Controller
-                name="password"
-                control={control}
-                rules={{
-                    required: true,
-                }}
-                render={({ field: { onChange, value } }) => (
-                    <TextInput
-                        style={style.form}
-                        placeholder="Password"
-                        onChangeText={onChange}
-                        value={value}
-                    />
-                )}
-            />
+            <ControlForm {...{ control, errors, name: 'login', rules: { required: 'Login is requred folder' }, placeholder: 'Login' }} />
+            <ControlForm {...{ control, errors, name: 'password', rules: { required: 'Password is requred folder' }, placeholder: 'Password' }} />
             <Button
                 title="Log in"
                 onPress={handleSubmit(onSubmit)}
             />
-            {/* <Controller/> */}
         </View>
     );
 };
--- src/Form/FormStyle.ts
+++ src/Form/FormStyle.ts
@@ -5,4 +5,12 @@ export const style = StyleSheet.create({
         margin: 10,
         borderWidth: 1,
     },
+    form_error: {
+        borderColor: 'red',
+    },
+    error_text: {
+        margin: 10,
+        marginTop: -2,
+        color: 'red',
+    },
 });
